c21a2feae56518a3a61620229d6b811dd9a80de9
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-12-11 19:27:27 +0530

    Adding Logo in Navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const Logo = () => {
+  return (
+    <Image
+      src="/images/logo.png"
+      alt="Logo"
+      height="100"
+      width="100"
+      className="hidden md:block cursor-pointer"
+    />
+  );
+};
+
+export default Logo;
