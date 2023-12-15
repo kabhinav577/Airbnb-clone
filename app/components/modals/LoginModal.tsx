620ea1f11ca273ccfaa6a017682df5adee6c2993
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-12-15 19:23:09 +0530

    feat: Finishing both Github and Google Authentication

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -84,7 +84,7 @@ const LoginModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn('google')}
       />
       <Button
         outline
