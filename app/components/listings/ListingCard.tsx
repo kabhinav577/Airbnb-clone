d48443f5e13ed24c71b7aab4454f88b5ebe50eeb
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-12-20 19:21:01 +0530

    feat: getting listings from server and show in Listing card component

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+interface ListingCardProps {}
+
+const ListingCard = () => {
+  return <div>ListingCard</div>;
+};
+
+export default ListingCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div className="text-3xl font-bold underline">Hello World!</div>;
+import getListings from './actions/getListings';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
+
+export default async function Home() {
+  const listings = await getListings();
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
+  return (
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => {
+          return <ListingCard key={listing.id} data={listing} />;
+        })}
+      </div>
+    </Container>
+  );
 }
