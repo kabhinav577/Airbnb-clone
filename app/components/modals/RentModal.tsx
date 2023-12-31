80292ce1c9bfa032c0675791239e649a52d367a2
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-12-31 18:51:52 +0530

    feat: protecting Routes and Dynamic label in Search comp

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -67,6 +67,7 @@ const RentModal = () => {
       dynamic(() => import('../Map'), {
         ssr: false,
       }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [location]
   );
 
